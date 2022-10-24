17db5a3aa4bfeae3469eab07bfd89e56b63305b9
carmenmm <109205891+carmenmm@users.noreply.github.com>
2022-10-24 20:49:44 +0200

    Delete 04_documentado/03_crud_libros_basedatos_03 directory

--- 04_documentado/03_crud_libros_basedatos_03/src/main/java/com/curso/dao/LibrosDao.java
+++
@@ -1,9 +0,0 @@
-package com.curso.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.curso.model.Libro;
-
-public interface LibrosDao extends JpaRepository<Libro, Integer> {
-
-}
